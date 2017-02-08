d12509b07ce2cfee4f668ccaf8c4b6ec2cb92b80
tobigels <tobi741236985@googlemail.com>
2017-02-08 15:44:35 +0100

    no message

---
+++ README.txt
@@ -0,0 +1,9 @@
+********************************************************************************
+*   Änderungen, Anmerkungen & Co.                                              *
+*   Luto, Lukas Völler, Tobias Gels, 2017                                      *
+'*******************************************************************************
+
+bean-discovery-mode in der beans.xml vol "all" auf "annotated"
+
+Textänderungen überall price nicht prize. Das eine ist der Preis den man
+bezahlt, der andere der Preis den man gewinnt.
--- src/java/package_Cookie/CookieView.java
+++ src/java/package_Cookie/CookieView.java
@@ -2,41 +2,65 @@ package package_Cookie;
  
 import java.awt.event.ActionEvent;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
+import javax.annotation.ManagedBean;
 import javax.faces.application.FacesMessage;
 import javax.inject.Named;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
  
-@ViewScoped
-@Named
-public class CookieView implements Serializable {
-     
-    private List<Cookie> cookies;
-         
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+//@Named
+//@ViewScoped
+@ManagedBean
+//@ViewScoped
+public class CookieView /*implements Serializable*/ {
+    
     @ManagedProperty("#{cookieService}")
     private CookieService service;
-     
-    @PostConstruct
-    public void init() {
-        //cookies = service.createCookies_Debug();
+    
+    private List<Cookie> cookies;
+    private String toAddName;
+    private double toAddPrice;
+    private int toAddCount;
+    
+    public void addCookieButton(ActionEvent actionEvent) {
+        addMessage("Cookie hinzugefügt");
     }
     
-    public void buttonAction(ActionEvent actionEvent) {
-        addMessage("Welcome to Primefaces!!");
+    public void deleteCookieButton(ActionEvent actionEvent) {
+        addMessage("Cookie gelöscht");
     }
-     
+
     public void addMessage(String summary) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
- 
-    public List<Cookie> getCookies() {
-        return cookies;
+    
+ /*         
+    public CookieView() {
+        this.cookies = new ArrayList<>();
+        
+        Cookie newCookieBlue = new Cookie(1, "blue", 2.49, 50);
+        Cookie newCookieGreen = new Cookie(2, "green", 2.49, 50);
+        Cookie newCookieMM = new Cookie(3, "mm", 3.49, 50);
+        Cookie newCookieNormal = new Cookie(4, "normal", 1.99, 50);
+        
+        this.cookies.add(newCookieBlue);
+        this.cookies.add(newCookieGreen);
+        this.cookies.add(newCookieMM);
+        this.cookies.add(newCookieNormal);
     }
-    public void setService(CookieService service) {
-        this.service = service;
-    }
+  */  
+    //public List<Cookie> getCookies() {
+    //    return cookies;
+    //}
+    
+    //public void setService(CookieService service) {
+    //    this.service = service;
+    //}
 }
